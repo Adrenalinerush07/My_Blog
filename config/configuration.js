9a2043403bf78197b747a56751e01d414c1cd2a3
cyberghost002 <49005095+cyberghost002@users.noreply.github.com>
2020-12-23 22:59:48 +0530

    1.0.1

---
+++ config/configuration.js
@@ -0,0 +1,32 @@
+
+// mongodb+srv://KartikJaiswal:<password>@blog.smyeg.mongodb.net/<dbname>?retryWrites=true&w=majority 
+// l13yFSWpb2YU1qR5
+// KartikJaiswal
+
+
+module.exports = {
+    mongoDbUrl : 'mongodb+srv://KartikJaiswal:l13yFSWpb2YU1qR5@blog.smyeg.mongodb.net/<dbname>?retryWrites=true&w=majority/KartiksBlog',
+    PORT: process.env.PORT || 3000,
+    globalVariables: (req, res, next) => {
+        res.locals.success_message = req.flash('success-message');
+        res.locals.error_message = req.flash('error-message');
+        res.locals.user = req.user || null;
+        next();
+    },
+
+
+};
+
+
+// module.exports = {
+//     mongoDbUrl : 'mongodb://localhost:27017/KartikBlog',
+//     PORT: process.env.PORT || 3000,
+//     globalVariables: (req, res, next) => {
+//         res.locals.success_message = req.flash('success-message');
+//         res.locals.error_message = req.flash('error-message');
+//         res.locals.user = req.user || null;
+//         next();
+//     },
+
+
+// };
---
+++ controllers/defaultController.js
@@ -0,0 +1,94 @@
+const Post = require('../models/PostModel').Post;
+const Category = require('../models/CategoryModel').Category;
+const bcrypt = require('bcryptjs');
+const User = require('../models/UserModel').User;
+
+
+module.exports = {
+
+    index: async (req, res) => {
+
+        const posts = await Post.find();
+        const categories = await Category.find();
+        res.render('default/index', {posts: posts, categories: categories});
+    },
+
+    /* LOGIN ROUTES */
+    loginGet: (req, res) => {
+        res.render('default/login', {message: req.flash('error')});
+    },
+
+
+    loginPost: (req, res) => {
+
+    },
+
+    /* REGISTER ROUTES*/
+
+    registerGet: (req, res) => {
+        res.render('default/register');
+    },
+
+    registerPost: (req, res) => {
+        let errors = [];
+
+        if (!req.body.firstName) {
+            errors.push({message: 'First name is mandatory'});
+        }
+        if (!req.body.lastName) {
+            errors.push({message: 'Last name is mandatory'});
+        }
+        if (!req.body.email) {
+            errors.push({message: 'Email field is mandatory'});
+        }
+        if (!req.body.password || !req.body.passwordConfirm) {
+            errors.push({message: 'Password field is mandatory'});
+        }
+        if (req.body.password !== req.body.passwordConfirm) {
+            errors.push({message: 'Passwords do not match'});
+        }
+
+        if (errors.length > 0) {
+            res.render('default/register', {
+                errors: errors,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
+            });
+        } else {
+            User.findOne({email: req.body.email}).then(user => {
+                if (user) {
+                    req.flash('error-message', 'Email already exists, try to login.');
+                    res.redirect('/login');
+                } else {
+                    const newUser = new User(req.body);
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            newUser.password = hash;
+                            newUser.save().then(user => {
+                                req.flash('success-message', 'You are now registered');
+                                res.redirect('/login');
+                            });
+                        });
+                    });
+                }
+            });
+        }
+    },
+
+    singlePost: (req, res) => {
+        const id = req.params.id;
+
+        Post.findById(id).then(post => {
+            if (!post) {
+                res.status(404).json({message: 'No Post Found'});
+            }
+            else {
+                res.render('default/singlePost', {post: post});
+            }
+        })
+    }
+
+};
+
