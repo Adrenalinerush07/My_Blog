f7e85f5548fe3447a6a912a0f59fea8aa486d99e
Kartik Jaiswal <49005095+Adrenalinerush07@users.noreply.github.com>
2020-12-24 02:18:20 +0530

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,29 +1,32 @@
+
+// mongodb+srv://KartikJaiswal:<password>@blog.smyeg.mongodb.net/<dbname>?retryWrites=true&w=majority 
+// l13yFSWpb2YU1qR5
+// KartikJaiswal
+
+
 module.exports = {
-
-    selectOption : function (status, options) {
-
-        return options.fn(this).replace(new RegExp('value=\"'+status+'\"'), '$&selected="selected"');
+    mongoDbUrl : 'mongodb+srv://KartikJaiswal:l13yFSWpb2YU1qR5@blog.smyeg.mongodb.net/MyBlogkartiks?retryWrites=true&w=majority',
+    PORT: process.env.PORT || 3000,
+    globalVariables: (req, res, next) => {
+        res.locals.success_message = req.flash('success-message');
+        res.locals.error_message = req.flash('error-message');
+        res.locals.user = req.user || null;
+        next();
     },
-
-    isEmpty: function (obj) {
-        for(let key in obj) {
-            if(obj.hasOwnProperty(key)) {
-                return false;
-            }
-        }
-
-        return true;
-    },
-
-    isUserAuthenticated: (req, res, next) => {
-         if (req.isAuthenticated()) {
-             next();
-         }
-         else {
-             res.redirect('/login');
-         }
-    }
-
 
 
 };
+
+
+// module.exports = {
+//     mongoDbUrl : 'mongodb://localhost:27017/KartikBlog',
+//     PORT: process.env.PORT || 3000,
+//     globalVariables: (req, res, next) => {
+//         res.locals.success_message = req.flash('success-message');
+//         res.locals.error_message = req.flash('error-message');
+//         res.locals.user = req.user || null;
+//         next();
+//     },
+
+
+// };
